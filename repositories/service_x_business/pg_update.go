1e12542edc93b6e344e74abc93253d39af0f81a1
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-19 13:34:11 -0500

    Parametro 3

--- repositories/service_x_business/pg_update.go
+++ repositories/service_x_business/pg_update.go
@@ -10,7 +10,7 @@ func Pg_Update(input_mqtt_service models.Mqtt_Service) error {
 
 	db := models.Conectar_Pg_DB()
 
-	query := `INSERT INTO bussinessr_service(idbusiness,idservice,pricing,typemoney,isavailable) (select * from unnest($1::int[], $2::int[],$4::decimal(8,2)[],$4::int[],$5::boolean[]))`
+	query := `INSERT INTO bussinessr_service(idbusiness,idservice,pricing,typemoney,isavailable) (select * from unnest($1::int[], $2::int[],$3::decimal(8,2)[],$4::int[],$5::boolean[]))`
 	if _, err := db.Exec(context.Background(), query, input_mqtt_service.Idbusiness_pg, input_mqtt_service.Idservice_pg, input_mqtt_service.Pricing_pg, input_mqtt_service.TypeMoney_pg, input_mqtt_service.Isavailable_pg); err != nil {
 		return err
 	}
