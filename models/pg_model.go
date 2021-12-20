eec5adb7cb09ed416ac1027382e80da6dc05a6a4
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-20 13:40:21 -0500

    Agregando la distancia

--- models/pg_model.go
+++ models/pg_model.go
@@ -13,6 +13,7 @@ type Pg_R_PaymentMethod struct {
 }
 
 type Pg_Found_All_Business struct {
+	Distance   float64        `json:"distance"`
 	Banner     string         `json:"banner"`
 	IDBusiness int            `json:"idbusiness"`
 	IsOpen     bool           `json:"isopen"`
