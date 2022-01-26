f8ed5e819429ac9435e61745b149923318ec7fee
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-01-26 00:52:10 -0500

    v1.10

--- repositories/business/pg_find_basic_data.go
+++ repositories/business/pg_find_basic_data.go
@@ -11,7 +11,7 @@ func Pg_Find_BasicData(idbusiness int) (models.Pg_BasicData_ToBusiness, error) {
 	var basic_data models.Pg_BasicData_ToBusiness
 
 	db := models.Conectar_Pg_DB()
-	q := "SELECT b.name,b.timezone,CASE WHEN now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))< concat(bsch.endtime,b.timezone)::time with time zone AND now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))> concat(bsch.starttime,b.timezone)::time with time zone THEN true ELSE false END FROM business as b JOIN businessschedule as bsch ON b.idbusiness=bsch.idbusiness WHERE bsch.idschedule=EXTRACT(ISODOW FROM (NOW()::timestamp at time zone CONCAT('UTC',(b.timezone::integer)::varchar(3)))) AND b.idbusiness=$1"
+	q := "SELECT b.uniquename,b.timezone,CASE WHEN now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))< concat(bsch.endtime,b.timezone)::time with time zone AND now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))> concat(bsch.starttime,b.timezone)::time with time zone THEN true ELSE false END FROM business as b JOIN businessschedule as bsch ON b.idbusiness=bsch.idbusiness WHERE bsch.idschedule=EXTRACT(ISODOW FROM (NOW()::timestamp at time zone CONCAT('UTC',(b.timezone::integer)::varchar(3)))) AND b.idbusiness=$1"
 	error_shown := db.QueryRow(context.Background(), q, idbusiness).Scan(&basic_data.Name, &basic_data.TimeZone, &basic_data.IsOpen)
 
 	if error_shown != nil {
@@ -27,7 +27,7 @@ func Pg_Find_BasicData_WithoutData(idbusiness int) (models.Pg_BasicData_ToBusine
 	var basic_data models.Pg_BasicData_ToBusiness
 
 	db := models.Conectar_Pg_DB()
-	q := "SELECT b.name,b.timezone,false FROM business as b WHERE b.idbusiness=$1"
+	q := "SELECT b.uniquename,b.timezone,false FROM business as b WHERE b.idbusiness=$1"
 	error_shown := db.QueryRow(context.Background(), q, idbusiness).Scan(&basic_data.Name, &basic_data.TimeZone, &basic_data.IsOpen)
 
 	if error_shown != nil {
