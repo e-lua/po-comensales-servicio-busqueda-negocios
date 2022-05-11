574d1d60f8e1761259188ba6e0deb59d45c3fec8
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-05-11 04:20:53 -0500

    v1.25

--- models/pg_model.go
+++ models/pg_model.go
@@ -83,8 +83,9 @@ type Pg_R_TypeFood_ToBusiness struct {
 }
 
 type Pg_BasicData_ToBusiness struct {
-	IsOpen     bool   `json:"isopen"`
-	Name       string `json:"name"`
-	Uniquename string `json:"uniquename"`
-	TimeZone   string `json:"timezone"`
+	IsOpen          bool   `json:"isopen"`
+	Name            string `json:"name"`
+	Uniquename      string `json:"uniquename"`
+	TimeZone        string `json:"timezone"`
+	Typesuscription int    `json:"typesuscription"`
 }
