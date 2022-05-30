fdb04c4c6133abcbf7d2de2ad8eba3eeabfddea4
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-05-29 19:18:35 -0500

    v1.30

--- repositories/business/pg_find_basic_data.go
+++ repositories/business/pg_find_basic_data.go
@@ -17,8 +17,8 @@ func Pg_Find_BasicData(idbusiness int) (models.Pg_BasicData_ToBusiness, error) {
 	var basic_data models.Pg_BasicData_ToBusiness
 
 	db := models.Conectar_Pg_DB()
-	q := "SELECT b.IVA,b.typesuscription,COALESCE(b.uniquename,'sin nombre'),COALESCE(b.name,'sin nombre'), COALESCE(b.timezone,'0'),COALESCE(CASE WHEN now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))< concat(bsch.endtime,b.timezone)::time with time zone AND now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))> concat(bsch.starttime,b.timezone)::time with time zone THEN true ELSE false END,'false') FROM business as b JOIN businessschedule as bsch ON b.idbusiness=bsch.idbusiness WHERE bsch.idschedule=EXTRACT(ISODOW FROM (NOW()::timestamp at time zone CONCAT('UTC',(b.timezone::integer)::varchar(3)))) AND b.idbusiness=$1"
-	error_shown := db.QueryRow(ctx, q, idbusiness).Scan(&basic_data.IVA, &basic_data.Typesuscription, &basic_data.Uniquename, &basic_data.Name, &basic_data.TimeZone, &basic_data.IsOpen)
+	q := "SELECT b.legalidentity,b.IVA,b.typesuscription,COALESCE(b.uniquename,'sin nombre'),COALESCE(b.name,'sin nombre'), COALESCE(b.timezone,'0'),COALESCE(CASE WHEN now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))< concat(bsch.endtime,b.timezone)::time with time zone AND now()::time at time zone CONCAT('UTC',(b.timezone::integer*-1)::varchar(3))> concat(bsch.starttime,b.timezone)::time with time zone THEN true ELSE false END,'false') FROM business as b JOIN businessschedule as bsch ON b.idbusiness=bsch.idbusiness WHERE bsch.idschedule=EXTRACT(ISODOW FROM (NOW()::timestamp at time zone CONCAT('UTC',(b.timezone::integer)::varchar(3)))) AND b.idbusiness=$1"
+	error_shown := db.QueryRow(ctx, q, idbusiness).Scan(&basic_data.Legalidentity, &basic_data.IVA, &basic_data.Typesuscription, &basic_data.Uniquename, &basic_data.Name, &basic_data.TimeZone, &basic_data.IsOpen)
 
 	if error_shown != nil {
 		return basic_data, error_shown
@@ -38,8 +38,8 @@ func Pg_Find_BasicData_WithoutData(idbusiness int) (models.Pg_BasicData_ToBusine
 	var basic_data models.Pg_BasicData_ToBusiness
 
 	db := models.Conectar_Pg_DB()
-	q := "SELECT b.IVA,b.typesuscription,COALESCE(b.uniquename,'sin nombre'),COALESCE(b.name,'sin nombre'),COALESCE(b.timezone,'0'),false FROM business as b WHERE b.idbusiness=$1"
-	error_shown := db.QueryRow(ctx, q, idbusiness).Scan(&basic_data.IVA, &basic_data.Typesuscription, &basic_data.Uniquename, &basic_data.Name, &basic_data.TimeZone, &basic_data.IsOpen)
+	q := "SELECT b.legalidentity,b.IVA,b.typesuscription,COALESCE(b.uniquename,'sin nombre'),COALESCE(b.name,'sin nombre'),COALESCE(b.timezone,'0'),false FROM business as b WHERE b.idbusiness=$1"
+	error_shown := db.QueryRow(ctx, q, idbusiness).Scan(&basic_data.Legalidentity, &basic_data.IVA, &basic_data.Typesuscription, &basic_data.Uniquename, &basic_data.Name, &basic_data.TimeZone, &basic_data.IsOpen)
 
 	if error_shown != nil {
 		return basic_data, error_shown
